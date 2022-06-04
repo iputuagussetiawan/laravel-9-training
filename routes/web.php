0ec5db0c87c47e1dbf6c67667a1468718a31dcd3
DESKTOP-DE46NCF\LENOVO <agussetiawaniputu@gmail.com>
2022-06-04 20:38:04 +0800

    Backend Home Page Slider Option

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::controller(AdminController::class)->group(function () {
 //Home Slider 
 Route::controller(HomeSliderController::class)->group(function () {
     Route::get('/home/slide', 'HomeSlider')->name('home.slide');
+    Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
 });
 
 Route::get('/dashboard', function () {
