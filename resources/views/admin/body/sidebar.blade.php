659340d440748d32b92f46d6567efb33b76cb154
DESKTOP-DE46NCF\LENOVO <agussetiawaniputu@gmail.com>
2022-06-04 19:36:01 +0800

    frontend setup

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -1,43 +1,39 @@
 <div class="vertical-menu">
-
     <div data-simplebar class="h-100">
         @php
-        $id = Auth::user()->id;
-        $adminData = App\Models\User::find($id);
+            $id = Auth::user()->id;
+            $adminData = App\Models\User::find($id);
         @endphp
-
         <!-- User details -->
         <div class="user-profile text-center mt-3">
             <div class="">
-                <img class="rounded-circle avatar-md" src="{{ (!empty($adminData->profile_image))? url('upload/admin_images/'.$adminData->profile_image):url('upload/no_image.jpg') }}"
+                <img class="rounded-circle avatar-md"
+                    src="{{ !empty($adminData->profile_image) ? url('upload/admin_images/' . $adminData->profile_image) : url('upload/no_image.jpg') }}"
                     alt="Header Avatar">
             </div>
             <div class="mt-3">
                 <h4 class="font-size-16 mb-1">Julia Hudda</h4>
-                <span class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i> Online</span>
+                <span class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i>
+                    Online</span>
             </div>
         </div>
-
         <!--- Sidemenu -->
         <div id="sidebar-menu">
             <!-- Left Menu Start -->
             <ul class="metismenu list-unstyled" id="side-menu">
                 <li class="menu-title">Menu</li>
-
                 <li>
                     <a href="index.html" class="waves-effect">
                         <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">3</span>
                         <span>Dashboard</span>
                     </a>
                 </li>
-
                 <li>
                     <a href="calendar.html" class=" waves-effect">
                         <i class="ri-calendar-2-line"></i>
                         <span>Calendar</span>
                     </a>
                 </li>
-    
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="ri-mail-send-line"></i>
@@ -48,7 +44,6 @@
                         <li><a href="email-read.html">Read Email</a></li>
                     </ul>
                 </li>
-
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="ri-layout-3-line"></i>
@@ -66,7 +61,6 @@
                                 <li><a href="layouts-colored-sidebar.html">Colored Sidebar</a></li>
                             </ul>
                         </li>
-
                         <li>
                             <a href="javascript: void(0);" class="has-arrow">Horizontal</a>
                             <ul class="sub-menu" aria-expanded="true">
@@ -79,8 +73,6 @@
                         </li>
                     </ul>
                 </li>
-
-               
             </ul>
         </div>
         <!-- Sidebar -->
