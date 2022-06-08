327c6ebe8d16c0a0b107b05f46a95a5cf41ebe3b
DESKTOP-DE46NCF\LENOVO <agussetiawaniputu@gmail.com>
2022-06-08 15:30:29 +0800

    Frontend Contact Form Setup Part 1

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Home\PortfolioController;
 use App\Http\Controllers\Home\BlogCategoryController;
 use App\Http\Controllers\Home\BlogController;
 use App\Http\Controllers\Home\FooterController;
+use App\Http\Controllers\Home\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -83,6 +84,11 @@ Route::controller(BlogController::class)->group(function () {
 Route::controller(FooterController::class)->group(function () {
     Route::get('/footer/setup', 'FooterSetup')->name('footer.setup');
     Route::post('/update/footer', 'UpdateFooter')->name('update.footer');
+});
+
+ // Contact All Route 
+ Route::controller(ContactController::class)->group(function () {
+    Route::get('/contact', 'Contact')->name('contact.me');
 });
 
 
