616781dee3c157fcebeb80e275e662ec3497fcb1
DESKTOP-DE46NCF\LENOVO <agussetiawaniputu@gmail.com>
2022-06-08 12:40:28 +0800

    Frontend Blog Show Setup Part 2

--- resources/views/frontend/home_all/home_blog.blade.php
+++ resources/views/frontend/home_all/home_blog.blade.php
@@ -19,7 +19,7 @@ $blogs = App\Models\Blog::latest()
                             <span class="date">{{ Carbon\Carbon::parse($item->created_at)->diffForHumans() }}
                             </span>
                             <h3 class="title"><a href="blog-details.html"> {{ $item->blog_title }} </a></h3>
-                            <a href="blog-details.html" class="read__more">Read mORe</a>
+                            <a href="{{ route('blog.details',$item->id) }}" class="read__more">Read More</a>
                         </div>
                     </div>
                 </div>
